2f2b2587c2dfad9b9fb13ad276ac1573ed98ef1e
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-09-04 15:18:27 -0700

    update vowels solution

--- scripts/vowels.py
+++ scripts/vowels.py
@@ -1,1 +1,19 @@
 # Write the solution for "Filter Vowels".
+
+# open file and read in the words
+with open("inputs/vowels.txt", "r") as f:
+    words = f.read().splitlines()
+
+vowels = {"a", "e", "i", "o", "u"}
+
+# filter out the vowels
+res = 0
+for i in words:
+    for j in i:
+        if j.lower() in vowels:
+            res += 1
+
+# write the output to a file
+file_name = f"vowels-{res}.txt"
+with open(f"outputs/{file_name}", "w") as f:
+    f.write("\n".join(words))
